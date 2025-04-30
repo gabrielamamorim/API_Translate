0f2d4fcf2e91ed1943e2b9faa5c38b89ebc04350
gabriela_m_amorim <gabimaciellopesamorim@gmail.com>
2025-04-30 12:15:47 -0300

    Commitando p/ depois terminar

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-async function traduzirTexto(texto) {
-  const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(texto)}&langpair=pt|en`;
+// Cria uma função assíncrona chamada traduzirTexto que recebe uma string texto como parâmetro
+async function traduzirTexto(texto) { 
+  // Cria uma const chamada url para armazenar a URL da API
+  // encodeURIComponent(texto) garante que caracteres especiais (como acentos ou espaços) sejam aceitos na URL.
+  // texto é o valor do parâmetro (q) que a função recebe (ex: "Olá mundo")
+  // langpair=pt|en significa que está traduzindo do português(pt) para o inglês(en).
+  const url = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(texto)}&langpair=pt|en`; 
+  // Cria uma const chamada responde que irá armazenar uma requisição
+  // fetch faz uma requisição(HTTP GET(Busca dados)) para a url especificada(API)
+  // await espera a resposta da URL(API)
   const response = await fetch(url);
+  // Uma const chamada data vai armazenar a resposta da API em um objeto JavaScript(JSON)
+  // *.json converte a resposta da API em formato JSON*
   const data = await response.json();
   return data.responseData.translatedText;
 }
